fc33c6200757399e9583339d155592a8e0fa2900
federicoBetti <federico2.betti@mail.polimi.it>
2018-11-28 16:22:58 +0100

    small modifications on hybrid

--- KNN/HybridRecommender.py
+++ KNN/HybridRecommender.py
@@ -24,7 +24,8 @@ class HybridRecommender(SimilarityMatrixRecommender, Recommender):
 
     RECOMMENDER_NAME = "ItemKNNCFRecommender"
 
-    def __init__(self, URM_train, ICM, recommeder_list, dynamic=False, d_weights=None, weights=None, URM_validation=None, sparse_weights=True):
+    def __init__(self, URM_train, ICM, recommeder_list, dynamic=False, d_weights=None, weights=None,
+                 URM_validation=None, sparse_weights=True):
         super(Recommender, self).__init__()
 
         # CSR is faster during evaluation
@@ -39,9 +40,13 @@ class HybridRecommender(SimilarityMatrixRecommender, Recommender):
         self.recommender_list = []
         self.weights = weights
 
+        self.normalize = None
+        self.topK = None
+        self.shrink = None
 
         for recommender in recommeder_list:
-            if recommender in [SLIM_BPR_Cython, MatrixFactorization_BPR_Cython, MatrixFactorization_FunkSVD_Cython, MatrixFactorization_AsySVD_Cython]:
+            if recommender in [SLIM_BPR_Cython, MatrixFactorization_BPR_Cython, MatrixFactorization_FunkSVD_Cython,
+                               MatrixFactorization_AsySVD_Cython]:
                 print("class recognized")
                 self.recommender_list.append(recommender(URM_train, URM_validation=URM_validation))
             elif recommender is ItemKNNCBFRecommender:
@@ -80,9 +85,11 @@ class HybridRecommender(SimilarityMatrixRecommender, Recommender):
 
         for knn, shrink, recommender in zip(topK, shrink, self.recommender_list):
             if recommender.__class__ is SLIM_BPR_Cython:
-                recommender.fit(old_similrity_matrix=old_similrity_matrix, epochs=epochs, force_compute_sim=force_compute_sim)
+                recommender.fit(old_similrity_matrix=old_similrity_matrix, epochs=epochs,
+                                force_compute_sim=force_compute_sim)
 
-            elif recommender.__class__ in [MatrixFactorization_BPR_Cython, MatrixFactorization_FunkSVD_Cython, MatrixFactorization_AsySVD_Cython]:
+            elif recommender.__class__ in [MatrixFactorization_BPR_Cython, MatrixFactorization_FunkSVD_Cython,
+                                           MatrixFactorization_AsySVD_Cython]:
                 recommender.fit(epochs=epochs, force_compute_sim=force_compute_sim)
 
             else:  # ItemCF, UserCF, ItemCBF
@@ -140,102 +147,20 @@ class HybridRecommender(SimilarityMatrixRecommender, Recommender):
 
         else:
             raise NotImplementedError
-
-            user_profile = self.URM_train.indices[self.URM_train.indptr[user_id]:self.URM_train.indptr[user_id + 1]]
-            user_ratings = self.URM_train.data[self.URM_train.indptr[user_id]:self.URM_train.indptr[user_id + 1]]
-
-            relevant_weights = self.W[user_profile]
-            scores = relevant_weights.T.dot(user_ratings)
+            #
+            # user_profile = self.URM_train.indices[self.URM_train.indptr[user_id]:self.URM_train.indptr[user_id + 1]]
+            # user_ratings = self.URM_train.data[self.URM_train.indptr[user_id]:self.URM_train.indptr[user_id + 1]]
+            #
+            # relevant_weights = self.W[user_profile]
+            # scores = relevant_weights.T.dot(user_ratings)
 
         scores = final_score
-        # rank items and mirror column to obtain a ranking in descending score
-        # ranking = scores.argsort()
-        # ranking = np.flip(ranking, axis=0)
 
-        # Sorting is done in three steps. Faster then plain np.argsort for higher number of items
-        # - Partition the data to extract the set of relevant items
-        # - Sort only the relevant items
-        # - Get the original item index
         relevant_items_partition = (-scores).argpartition(n)[0:n]
         relevant_items_partition_sorting = np.argsort(-scores[relevant_items_partition])
         ranking = relevant_items_partition[relevant_items_partition_sorting]
 
         return ranking
-
-        # If is a scalar transform it in a 1-cell array
-        if np.isscalar(user_id_array):
-            user_id_array = np.atleast_1d(user_id_array)
-            single_user = True
-        else:
-            single_user = False
-
-        if cutoff is None:
-            cutoff = self.URM_train.shape[1] - 1
-
-        # Compute the scores using the model-specific function
-        # Vectorize over all users in user_id_array
-        scores_batch = self.compute_item_score(user_id_array)
-
-        # if self.normalize:
-        #     # normalization will keep the scores in the same range
-        #     # of value of the ratings in dataset
-        #     user_profile = self.URM_train[user_id]
-        #
-        #     rated = user_profile.copy()
-        #     rated.data = np.ones_like(rated.data)
-        #     if self.sparse_weights:
-        #         den = rated.dot(self.W_sparse).toarray().ravel()
-        #     else:
-        #         den = rated.dot(self.W).ravel()
-        #     den[np.abs(den) < 1e-6] = 1.0  # to avoid NaNs
-        #     scores /= den
-
-
-        for user_index in range(len(user_id_array)):
-
-            user_id = user_id_array[user_index]
-
-            if remove_seen_flag:
-                scores_batch[user_index, :] = self._remove_seen_on_scores(user_id, scores_batch[user_index, :])
-
-                # Sorting is done in three steps. Faster then plain np.argsort for higher number of items
-                # - Partition the data to extract the set of relevant items
-                # - Sort only the relevant items
-                # - Get the original item index
-                # relevant_items_partition = (-scores_user).argpartition(cutoff)[0:cutoff]
-                # relevant_items_partition_sorting = np.argsort(-scores_user[relevant_items_partition])
-                # ranking = relevant_items_partition[relevant_items_partition_sorting]
-                #
-                # ranking_list.append(ranking)
-
-        if remove_top_pop_flag:
-            scores_batch = self._remove_TopPop_on_scores(scores_batch)
-
-        if remove_CustomItems_flag:
-            scores_batch = self._remove_CustomItems_on_scores(scores_batch)
-
-        # scores_batch = np.arange(0,3260).reshape((1, -1))
-        # scores_batch = np.repeat(scores_batch, 1000, axis = 0)
-
-        # relevant_items_partition is block_size x cutoff
-        relevant_items_partition = (-scores_batch).argpartition(cutoff, axis=1)[:, 0:cutoff]
-
-        # Get original value and sort it
-        # [:, None] adds 1 dimension to the array, from (block_size,) to (block_size,1)
-        # This is done to correctly get scores_batch value as [row, relevant_items_partition[row,:]]
-        relevant_items_partition_original_value = scores_batch[
-            np.arange(scores_batch.shape[0])[:, None], relevant_items_partition]
-        relevant_items_partition_sorting = np.argsort(-relevant_items_partition_original_value, axis=1)
-        ranking = relevant_items_partition[
-            np.arange(relevant_items_partition.shape[0])[:, None], relevant_items_partition_sorting]
-
-        ranking_list = ranking.tolist()
-
-        # Return single list for one user, instead of list of lists
-        if single_user:
-            ranking_list = ranking_list[0]
-
-        return ranking_list
 
     def change_weights(self, level, pop):
         if level < pop[0]:
